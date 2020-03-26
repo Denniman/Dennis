5b83ec086e253f8782419c124c9b5da8bfebe484
Dennis <dennimanadams@gmail.com>
2020-03-26 06:36:25 +0100

    display name and courses

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ let courses = ['HTML', 'CSS', 'javaScript']
 for(let i = 1; i <= 200; i++) {
     if(courses.length % 2 === 0 && i % 2 === 0) {
         console.log(i)
+        console.log(name)
+        console.log(courses)
     } else if(courses.length % 2 !== 0 && i % 2 !== 0) {
         console.log(i)
+        console.log(name)
+        console.log(courses)
     }
 }
